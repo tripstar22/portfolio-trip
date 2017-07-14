8001d97c646bb91baaa82061405a9139f8354bf0
Trip Pruitt <trip@iconologic.com>
2017-07-14 14:02:57 -0400

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+// components
 import { AppComponent } from './app.component';
 import { HomeShellComponent } from './components/home-shell/home-shell.component';
 import { WorkDetailShellComponent } from './components/work-detail-shell/work-detail-shell.component';
@@ -10,6 +12,9 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/home-shell/about/about.component';
 import { SkillsComponent } from './components/home-shell/about/skills/skills.component';
 import { WorkComponent } from './components/home-shell/work/work.component';
+
+// routing
+import { routing } from './routing/app.routing';
 
 @NgModule({
 	declarations: [
@@ -24,7 +29,8 @@ import { WorkComponent } from './components/home-shell/work/work.component';
 		WorkComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		routing
 	],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing/app.routing.ts
@@ -0,0 +1,11 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeShellComponent } from '../components/home-shell/home-shell.component';
+
+const APP_ROUTES: Routes = [
+	{
+		path: '',
+		component: HomeShellComponent
+	}
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES, { useHash: true });
