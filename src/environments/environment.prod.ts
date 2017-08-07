11ddf95ca9fe87d172f43757384dc78d2cebf9eb
Trip Pruitt <trip@iconologic.com>
2017-08-07 15:05:22 -0400

    added firebase authentication to enviornment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyC5pBmYKhO6tpVq7jJ7oHfkJTlsDCIiDLQ',
+		authDomain: 'portfolio-ccfe7.firebaseapp.com',
+		databaseURL: 'https://portfolio-ccfe7.firebaseio.com',
+		projectId: 'portfolio-ccfe7',
+		storageBucket: 'portfolio-ccfe7.appspot.com',
+		messagingSenderId: '414511297502'
+	}
 };
